a753c4ee76c4f783b977f055b87342f9c9fb982c
Satya Fariz <satyafariznur@gmail.com>
2023-05-02 00:48:57 +0700

    add sorted color visualization

--- src/algorithms/heapSort.ts
+++ src/algorithms/heapSort.ts
@@ -5,19 +5,22 @@ const SECONDARY_COLOR = 'blue'
 const SORTED_COLOR = 'purple'
 
 const heapSort = (array: number[]): Animation[] => {
+  const sorted: ColorMap = {}
   const animations: Animation[] = []
 
   let lastParentNode = Math.floor(array.length / 2 - 1)
   let lastChild = array.length - 1
 
   while(lastParentNode >= 0) {
-    heapify(array, array.length, lastParentNode, animations)
+    heapify(array, array.length, lastParentNode, animations, sorted)
     lastParentNode--
   }
 
   while(lastChild >= 0) {
+    sorted[lastChild] = SORTED_COLOR
     animations.push({
       colors: {
+        ...sorted,
         [0]: PRIMARY_COLOR,
         [lastChild]: SECONDARY_COLOR
       }
@@ -27,20 +30,27 @@ const heapSort = (array: number[]): Animation[] => {
 
     animations.push({
       colors: {
+        ...sorted,
         [0]: SECONDARY_COLOR,
         [lastChild]: PRIMARY_COLOR
       },
       array: array.slice()
     })
 
-    heapify(array, lastChild, 0, animations)
+    heapify(array, lastChild, 0, animations, sorted)
     lastChild--
   }
+
+  animations.push({
+    colors: {
+      ...sorted
+    }
+  })
 
   return animations
 }
 
-const heapify = (array: number[], length: number, parentIdx: number, animations: Animation[]) => {
+const heapify = (array: number[], length: number, parentIdx: number, animations: Animation[], sorted: ColorMap) => {
   let largest = parentIdx
   const left = parentIdx * 2 + 1
   const right = left + 1
@@ -56,6 +66,7 @@ const heapify = (array: number[], length: number, parentIdx: number, animations:
   if(largest !== parentIdx) {
     animations.push({
       colors: {
+        ...sorted,
         [parentIdx]: PRIMARY_COLOR,
         [largest]: SECONDARY_COLOR
       }
@@ -64,12 +75,13 @@ const heapify = (array: number[], length: number, parentIdx: number, animations:
     [array[parentIdx], array[largest]] = [array[largest], array[parentIdx]]
     animations.push({
       colors: {
+        ...sorted,
         [parentIdx]: SECONDARY_COLOR,
         [largest]: PRIMARY_COLOR
       },
       array: array.slice()
     })
-    heapify(array, length, largest, animations)
+    heapify(array, length, largest, animations, sorted)
   }
 }
 
